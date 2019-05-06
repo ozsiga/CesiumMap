b792d56db9733f1f1ac1a27252eb2814fe13fe63
Hajkó Balázs <bhajko@gmail.com>
2019-05-06 09:20:48 +0200

    map center added

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ Cesium.Ion.defaultAccessToken =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmYjFmZWU0Ny03NDE4LTQyZDYtYTY4YS0zMTM2MjMxMTU1MTgiLCJpZCI6MTA1NzYsInNjb3BlcyI6WyJhc2wiLCJhc3IiLCJhc3ciLCJnYyJdLCJpYXQiOjE1NTY4ODMyNzZ9.aAZSMZ3_AQ_aTxUKC87VbAlJw_orNRMoUDZTVk-uRSE';
 
 const viewer = new Cesium.Viewer('cesiumContainer');
+
+let center = Cesium.Cartesian3.fromDegrees(17.605, 46.985);
+viewer.camera.lookAt(center, new Cesium.Cartesian3(0.0, 0.0, 2000));
