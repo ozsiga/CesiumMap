16effe676dc8e0e761409431a79c07740378d68e
Hajkó Balázs <bhajko@gmail.com>
2019-05-06 08:49:40 +0200

    file structure

---
+++ index.js
@@ -0,0 +1,4 @@
+Cesium.Ion.defaultAccessToken =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmYjFmZWU0Ny03NDE4LTQyZDYtYTY4YS0zMTM2MjMxMTU1MTgiLCJpZCI6MTA1NzYsInNjb3BlcyI6WyJhc2wiLCJhc3IiLCJhc3ciLCJnYyJdLCJpYXQiOjE1NTY4ODMyNzZ9.aAZSMZ3_AQ_aTxUKC87VbAlJw_orNRMoUDZTVk-uRSE';
+
+const viewer = new Cesium.Viewer('cesiumContainer');
